f4ab8ec97eb4e42231eb6b16ff19de79b5a8bcf5
Willem Gorisse <willem.gorisse@mendix.com>
2017-02-01 17:43:20 +0100

    updated version, a lot of extra work and improvements. Added keyboard and focus working on disabling / enabling

--- src/DropdownSelector/widget/DropdownSelector.js
+++ src/DropdownSelector/widget/DropdownSelector.js
@@ -65,7 +65,7 @@ define([
         _optionArray: null,
         _newOptionDomArray: null,
         _selectedIndex: null,
-        _allSelectDropdowns: {},
+        _goingOnce: null,
 
         // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
         constructor: function () {
@@ -81,7 +81,6 @@ define([
         postCreate: function () {
             logger.debug(this.id + ".postCreate");
             this.targetName = ".mx-name-" + this.targetName;
-            this._allSelectDropdowns[this.id] = this;
         },
 
         // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
@@ -97,6 +96,8 @@ define([
                 //dojoClass.add(this._formGroupNode, "relative"); //TODO check if this is needed
                 this._selectNode = dojoQuery('select',this._formGroupNode)[0];
             } else {
+                logger.debug("there was no Mendix widget found with the specified target name");
+                callback();
                 return;
             }
             if (this._selectNode) {
@@ -112,19 +113,50 @@ define([
                     this._optionArray.push(optionElement);
                 }));
             } else {
+                logger.debug("there was no select element found withing the specified Mendix widget");
+                callback();
                 return;
             }
-            this._resetSubscriptions(callback);
+            this._resetSubscriptions();
+            this._updateRendering(callback());
         },
 
         // mxui.widget._WidgetBase.enable is called when the widget should enable editing. Implement to enable editing if widget is input widget.
         enable: function () {
-          logger.debug(this.id + ".enable");
+            console.log("want to enable editing");
+            logger.debug(this.id + ".enable");
+            switch(this.renderingMode){
+                case "full":
+
+                    dojoConstruct.place(this.domNode,this._formGroupNode,1);
+                    break;
+                case "default":
+                default:
+                    break;
+            }
         },
 
         // mxui.widget._WidgetBase.enable is called when the widget should disable editing. Implement to disable editing if widget is input widget.
         disable: function () {
-          logger.debug(this.id + ".disable");
+            console.log("want to disable editing");
+            logger.debug(this.id + ".disable");
+            switch(this.renderingMode) {
+                case "full":
+                    //dojoStyle.set(this.domNode,"display","none");
+                    // TODO: testing destroy as a way
+                    if ( this._eventHandles) {
+                        dojoArray.forEach( this._eventHandles, dojoLang.hitch(this, function( eventHandle){
+                            this.disconnect(eventHandle);
+                        }));
+                        this._eventHandles = [];
+                    }
+                    dojoConstruct.destroy(this.domNode);
+                    break;
+                case "default":
+                default:
+                    // do nothing, only relevant for versions where
+                    break;
+            }
         },
 
         // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
@@ -135,6 +167,7 @@ define([
         // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
         uninitialize: function () {
           logger.debug(this.id + ".uninitialize");
+          this._unsubscribe();
             // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
         },
 
@@ -153,28 +186,32 @@ define([
              switch(this.renderingMode) {
                 case "full":
                     // set event to the button
-                    this.connect(this.selectDropdownButton, "click", dojoLang.hitch(this,this._clickedDropdown));
+                    this._eventHandles.push(this.connect(this.selectDropdownButton, "click", dojoLang.hitch(this,this._clickedDropdown)));
+                    this._eventHandles.push(this.connect(this.selectDropdownButton, "keydown", dojoLang.hitch(this,this._buttonKeyDown)));
 
                     // set event to window for automatically closing the
-                    this.connect(document, "click", dojoLang.hitch(this,function(event){
-                        // if a widget external click is made: close the menu if open
-                        var isOpenString = dojoAttr.get(this.selectDropdownButton,"aria-expanded")
-                        if (isOpenString == "true") {
-                            this._toggleDropdown(isOpenString);
+                    this._eventHandles.push(this.connect(document, "click", dojoLang.hitch(this,function(event){
+                        // if a widget external click is made: close the menu if open. first check though if the window click is not bubbled from this instance
+                        if (!this._formGroupNode.contains(event.target)) {
+                            var isOpenString = dojoAttr.get(this.selectDropdownButton,"aria-expanded")
+                            if (isOpenString == "true") {
+                                this._toggleDropdown(isOpenString);
+                            }
                         }
-                    }));
+                    })));
 
                     // set events to the listitems
                     dojoArray.forEach(this._newOptionDomArray, dojoLang.hitch(this, function(listItem){
                         var link = dojoTraverse(listItem).children()[0];
-                        this.connect(link, "click", dojoLang.hitch(this, this._listItemClicked))
+                        this._eventHandles.push(this.connect(link, "click", dojoLang.hitch(this, this._listItemClicked)));
+                        this._eventHandles.push(this.connect(link, "keydown", dojoLang.hitch(this,this._optionLinkKeyDown)));
                     }));
                     break;
                 case "default":
                 default:
                     this._eventHandles.push(
                         this.connect(this._selectNode, "click", dojoLang.hitch(this, function(e){
-                            console.log("just clicked the bitch");
+                        
                         }))
                     );
 
@@ -221,17 +258,23 @@ define([
             
             // The callback, coming from update, needs to be executed, to let the page know it finished rendering
             mendix.lang.nullExec(callback);
+            console.log("just did the callback, so finished to loop");
         },
 
         // Rerender the interface.
         _updateRendering: function (callback) {
             switch(this.renderingMode) {
                 case "full":
-                    console.log("full rendering enabled");
-
-                    //  this.selectValueFieldNode.placeholder = this.placeholderText;
-                    // move the custom widget dom 
+                    // move the custom widget dom
                     dojoConstruct.place(this.domNode,this._formGroupNode,1);
+
+                    // if listitems allready exist: remove them.
+                    if (this._newOptionDomArray && this._newOptionDomArray.length > 0 ) {
+                        dojoArray.forEach(this._newOptionDomArray,dojoLang.hitch(this,function(optionDom) {
+                            dojoConstruct.destroy(optionDom);
+                        }));
+                        this._newOptionDomArray = [];
+                    }
 
                     // create the list items
                     dojoArray.forEach(this._optionArray,dojoLang.hitch(this, function(option){
@@ -239,19 +282,26 @@ define([
                     }));
 
                     // set the buttontext and styling
-                    dojoAttr.set(this.selectValueFieldNode,"data-placeholder", this.placeholderText);
-                    this._setSelectButton (this.selectValueFieldNode);
+                    dojoAttr.set(this.selectValueFieldNode,'data-placeholder', this.placeholderText);
+                    this._setSelectButton();
 
+                    // hide the original select                         
+                    //dojoAttr.set(this._selectNode,"style","display:none;");
+                    var selectWrapper = dojoTraverse(this._selectNode).parent()[0];
+                    dojoAttr.set(this._selectNode,"tabindex","-1");
+                    dojoStyle.set(this._selectNode,"display","none");
                     break;
                 case "default":
                 default:
                     // destroy the template since we won't be using it
                     dojoConstruct.destroy(this.domNode);
+
                     // create the placeholder functionality in original option
                     if (this._optionDomArray[0]){
                         this._optionDomArray[0].innerHTML = this.placeholderText;
                     }
-                    console.log("default only placeholder selected");
+                    // adjust the button
+                    this._setSelectButton();
                     break;
             }
             
@@ -269,7 +319,7 @@ define([
                 selected = true;
             }
             // create the html of the new listitem.
-            newLinkNode = dojoConstruct.create("a", {role:"option", "data-value":option.value, innerHTML: option.text});
+            newLinkNode = dojoConstruct.create("a", {role:"option", "data-value":option.value, innerHTML: option.text, tabindex:"0"});
             if (selected) {
                 dojoAttr.set(newLinkNode, "aria-selected", "selected");
             }
@@ -284,29 +334,95 @@ define([
             this._newOptionDomArray.push(newListItemNode);
         },
 
-        _setSelectButton: function(selectButton) {
+        _setSelectButton: function() {
             if (this._selectedIndex == 0) {
                 // no item is selected - placeholder functionality
-                //selectButton.innerHTML = dojoAttr.get(selectButton,'data-placeholder');
-                dojoClass.add(selectButton,'placeholder');
+                this.selectValueFieldNode.textContent = dojoAttr.get(this.selectValueFieldNode,'data-placeholder');
+                dojoClass.add(this.selectValueFieldNode,'placeholder');
             } else {
                 // we have a selected item
                 var selectedOption = this._optionArray[this._selectedIndex];
-                selectButton.innerHTML = selectedOption.text;
-                dojoClass.remove(selectButton,'placeholder');
+                this.selectValueFieldNode.textContent = selectedOption.text; 
+                dojoClass.remove(this.selectValueFieldNode,'placeholder');
             }
         },
 
         _clickedDropdown: function(event) {
-            event.stopPropagation();
+            console.log("clicked the dropdown");
             var isOpenString = dojoAttr.get(event.currentTarget,"aria-expanded");
-            // check if the menu is opening up: if so: close all the others
-            if (isOpenString == "false") {
-                this._closeAllDropDowns();
-            }
             // toggle the menu
             this._toggleDropdown(isOpenString);
-            console.log ('just clicked the input field hoor');
+        },
+
+        _buttonKeyDown: function(event) {
+            // 40 = keydown, 38 = keyup, 13= enter, 27 = escape, 38|40|27|32
+            var keyCode = event.which;
+            switch (keyCode) {
+                case 40:
+                case 38:
+                case 13:
+                case 32:
+                    event.stopPropagation();
+                    event.preventDefault();
+                    this._toggleDropdown("false");
+                    break;
+                default:
+                    break;
+            }
+        },
+
+        _optionLinkKeyDown: function(event) {
+            event.stopPropagation();
+            event.preventDefault();
+            var keyCode = event.which;
+
+            var listItem = dojoTraverse(event.currentTarget).parent()[0];
+            var currentIndex = Number(dojoAttr.get(listItem,"data-original-index"));
+
+            switch (keyCode) {
+                case 40:
+                    // keydown
+                    if (currentIndex < (this._optionArray.length - 1)) {
+                       this._setFocusOnListItem(currentIndex + 1);
+                    }
+                    break;
+                case 38:
+                    //key up
+                    if (currentIndex > 0) {
+                        this._setFocusOnListItem(currentIndex - 1);
+                    }
+                    break;
+                case 13:
+                case 32:
+                    // enter or space
+                    if (currentIndex != this._selectedIndex) {
+                        this._setNewIndex(currentIndex);
+                    }
+                    this._toggleDropdown("true");
+                    this._setFocusOnButton();
+                    break;
+                case 27:
+                    // escape
+                    this._setFocusOnButton();
+                    this._toggleDropdown("true");
+                    break;
+                default:
+                    break;
+            }
+        },
+
+        _setFocusOnListItem: function(index) {
+            var selectedListItem = this._newOptionDomArray[index];
+            var newLink = selectedListItem.querySelector('a');
+            // remove old focus
+            document.activeElement.blur();
+            // set new focus
+            newLink.focus();
+        },
+
+        _setFocusOnButton: function() {
+            document.activeElement.blur();
+            this.selectDropdownButton.focus();
         },
 
         _toggleDropdown: function(isOpenString) {
@@ -314,28 +430,11 @@ define([
                 // closed menu - so open it up
                 dojoClass.add(this.domNode, "open");
                 dojoAttr.set(this.selectDropdownButton,"aria-expanded","true");
+                this._setFocusOnListItem(this._selectedIndex);
             } else {
                 // open menu - so close it
                 dojoClass.remove(this.domNode, "open");
                 dojoAttr.set(this.selectDropdownButton,"aria-expanded","false");
-            }
-        },
-
-        // method meant for if more than one select field is present in the page and they need to close each other
-        _closeAllDropDowns: function() {
-            var dropdown = null;
-
-            for(dropdown in this._allSelectDropdowns) {
-                if (this._allSelectDropdowns.hasOwnProperty(dropdown) && dropdown !== this.id){
-                    var dropdownMenu = this._allSelectDropdowns[dropdown];
-                    if (dropdownMenu.hasOwnProperty("selectDropdownButton")) {
-                        var isOpenString = dojoAttr.get(dropdownMenu.selectDropdownButton,"aria-expanded");
-                        if (isOpenString == "true") {
-                            dojoClass.remove(dropdownMenu.domNode,"open");
-                            dojoAttr.set(dropdownMenu.selectDropdownButton,"aria-expanded","false"); 
-                        }
-                    }
-                }     
             }
         },
 
@@ -345,11 +444,15 @@ define([
 
             listItem = dojoTraverse(event.currentTarget).parent()[0];
             newIndex = Number(dojoAttr.get(listItem,"data-original-index"));
-            this._setNewIndex(newIndex);
+            if (newIndex != this._selectedIndex){
+                this._setNewIndex(newIndex);
+            }
+
+            // close the actual dropdown as the window event isn't doing anything
+            this._toggleDropdown("true");
         },
 
         _setNewIndex: function(newIndex) {
-            console.log("setting new index to: " + newIndex);
             // first adjust the new dropdown - begin with removing the older one
             var oldListItem = this._newOptionDomArray[this._selectedIndex],
                 oldListLink = dojoTraverse(oldListItem).children()[0],
@@ -366,22 +469,27 @@ define([
             dojoClass.add(newListItem,"selected");
             dojoAttr.set(newListLink,"aria-selected","true");
             // set the button text and style
-            this._setSelectButton(this.selectDropdownButton);
-            
+            this._setSelectButton();
+
+            // adjust original select element NOTE TODO: if same value don't trigger event.
+            this._selectNode.options.selectedIndex = this._selectedIndex;
+            var newEvent = new Event('change');
+            this._selectNode.dispatchEvent(newEvent);
         },
 
         _unsubscribe: function () {
           if (this._handles) {
-              dojoArray.forEach(this._handles, function (handle) {
-                  mx.data.unsubscribe(handle);
-              });
+              dojoArray.forEach(this._handles, dojoLang.hitch(this,function (handle) {
+                  mx.data.unsubscribe(handle); 
+                  //this._unsubscribe(handle);
+              }));
               this._handles = [];
           }
 
           if ( this._eventHandles) {
-              dojoArray.forEach( this._eventHandles,function( eventHandle){
+              dojoArray.forEach( this._eventHandles, dojoLang.hitch(this, function( eventHandle){
                   this.disconnect(eventHandle);
-              });
+              }));
                this._eventHandles = [];
           }
 
@@ -389,7 +497,7 @@ define([
         },
 
         // Reset subscriptions.
-        _resetSubscriptions: function (callback) {
+        _resetSubscriptions: function () {
             logger.debug(this.id + "._resetSubscriptions");
             // Release handles on previous object, if any.
             this._unsubscribe();
@@ -399,13 +507,11 @@ define([
                 var objectHandle = mx.data.subscribe({
                     guid: this._contextObj.getGuid(),
                     callback: dojoLang.hitch(this, function (guid) {
-                        this._updateRendering();
+                        this.update(this._contextObj,function(){});
                     })
                 });
                 this._handles = [ objectHandle ];
             }
-            
-            this._updateRendering(callback);
         }
     });
 });
